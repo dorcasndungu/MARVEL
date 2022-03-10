11b3f0ce0df9121f52569f378f3083703f72be94
dorcasndungu <ndungumwihaki017@gmail.com>
2022-03-10 16:30:09 +0300

    final commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,8 +16,20 @@ import static spark.route.HttpMethod.post;
 public class App {
     public static void main(String[] args) {
 
-        staticFileLocation("/public");
 
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+
+        // This tells our app that if Heroku sets a port for us, we need to use that port.
+        // Otherwise, if they do not, continue using port 4567.
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+        staticFileLocation("/public");
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             request.session().attribute("squads",Squad.getSquads());
